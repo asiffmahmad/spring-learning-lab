098f94493de08b20a82f7212e23962c48451b750
asiff <asiffmahmad9@gmail.com>
2025-02-02 23:24:06 +0530

    new controller added

---
+++ src/main/java/com/learnspring/lab/core/SpringCoreController.java
@@ -0,0 +1,13 @@
+package com.learnspring.lab.core;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class SpringCoreController {
+	
+	
+	
+
+}
